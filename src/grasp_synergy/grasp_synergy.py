0d506b6204b01840bc608937e69ed75a8458e48e
Felix Duvallet <felix.duvallet@epfl.ch>
2016-04-04 11:34:12 +0200

    Implemented grasp synergy learning.

--- src/grasp_synergy/grasp_synergy.py
+++ src/grasp_synergy/grasp_synergy.py
@@ -1,10 +1,72 @@
+import rospy
+
+import numpy as np
 from sklearn.decomposition import PCA
 
 
 class GraspSynergy(object):
+    """
+    A grasp synergy (aka eigengrasp) is a lower-dimensional representation of
+    the hand configuration useful to simplify all manners of grasping things.
+
+    Essentially this is just a utilitarian wrapper around PCA.
+    """
 
     def __init__(self):
-        self._pca = PCA(n_components=None)  # Use *all* components.
+        # Fit *all* components. Select lower-dimensional subspace later.
+        self._pca = PCA(n_components=None)
+        self._N = 0
+        self._D = 0
+
+        # Store the transformed joint angles for all principal components. This
+        # is used to compute the min/max values of the transformed
+        # representation.
+        self._transformed_joint_angles = None
+
+    def fit_joint_values(self, joint_values):
+        """
+        Fit the principal components of the given joint values to compute the
+        grasp synergies (aka eigengrasps).
+
+        We compute and store *all* D principal components here. The
+        dimensionality of the lower-dimensional subspace is determined at grasp
+        computation time.
+
+        :param joint_values A numpy array (N by D) with N datapoints of D
+        dimensions.
+
+        """
+        self._N = joint_values.shape[0]
+        self._D = joint_values.shape[1]
+        self._transformed_joint_angles = self._pca.fit_transform(joint_values)
+
+    def fit_joint_state_messages(self, joint_states_msgs):
+        """
+        Extract joint states from a list of ROS messages, then compute the grasp
+        synergies.
+
+        :param joint_states_msgs A list of ROS sensor_msgs/JointState.
+        """
+
+        joint_values = []
+        for (idx, msg) in enumerate(joint_states_msgs):
+            try:  # Catch any exceptions due to a wrong message type.
+                joint_values.append(msg.position)
+            except AttributeError:
+                rospy.logwarn('Message is not a JointState, skipping.')
+                pass
+        # Create a numpy array of size NxD.
+        joint_values = np.array(joint_values)
+
+        return self.fit_joint_values(joint_values)
+
+    def compute_grasp(self, alphas):
+        if not hasattr(self._pca, 'n_components_'):
+            rospy.logwarn('No grasp synergies, did you call fit_joint_*?')
+            return None
+        pass
+
+
 
 
     pass
--- src/grasp_synergy/test/test_synergy.py
+++ src/grasp_synergy/test/test_synergy.py
@@ -1,4 +1,7 @@
 import unittest
+import os
+import rosbag
+import numpy as np
 
 from grasp_synergy.grasp_synergy import GraspSynergy
 
@@ -6,11 +9,42 @@ from grasp_synergy.grasp_synergy import GraspSynergy
 class TestCase(unittest.TestCase):
 
     def setUp(self):
+        (_, self.messages, _) = zip(*self.data)
+        self.synergy = GraspSynergy()
+        pass
+
+    @classmethod
+    def setUpClass(cls):
+        fpath = os.path.join(os.path.dirname(__file__), 'data', 'allegro.bag')
+        bag = rosbag.Bag(fpath)
+        topics = ['/allegroHand_0/joint_states']
+        cls.data = list(bag.read_messages(topics))
+        bag.close()
         pass
 
     def test_constructor(self):
-        gs = GraspSynergy()
-        self.assertIsNotNone(gs)
+        self.assertIsNotNone(self.synergy)
+        self.assertEqual(0, self.synergy._D)
+        self.assertEqual(0, self.synergy._N)
+
+    def test_fit_joint_values(self):
+        joints = np.random.random((25, 5))
+        self.synergy.fit_joint_values(joints)
+        self.assertEqual(5, self.synergy._D)
+        self.assertEqual(25, self.synergy._N)
+        self.assertEqual(5, self.synergy._pca.n_components_)
+
+    def test_fit_joint_messages(self):
+        self.synergy.fit_joint_state_messages(self.messages)
+        self.assertEqual(16, self.synergy._D)
+        self.assertEqual(82, self.synergy._N)
+        self.assertEqual(16, self.synergy._pca.n_components_)
+
+    def test_compute_grasp_no_data(self):
+        ret = self.synergy.compute_grasp([0, 1, 3])
+        self.assertIsNone(ret)
+
+
 
 
 if __name__ == '__main__':
