627ceece62360cfe744839bac22f39068d99e5d5
Felix Duvallet <felix.duvallet@epfl.ch>
2016-04-04 10:14:20 +0200

    Set up empty grasp synergy package.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+d = generate_distutils_setup(
+    packages=['grasp_synergy'],
+    package_dir={'': 'src'},
+    requires={'rospy', 'python-sklearn'}
+)
+
+setup(**d)
