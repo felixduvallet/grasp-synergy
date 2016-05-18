6a6837a65b4eb687794f124c1313020db1b3466a
Felix Duvallet <felix.duvallet@epfl.ch>
2016-05-18 14:57:35 +0200

    Remove sklearn from setup.py; causing ros buildfarm to fail.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 d = generate_distutils_setup(
     packages=['grasp_synergy'],
     package_dir={'': 'src'},
-    requires={'rospy', 'python-sklearn'}
+    requires={'rospy'}
 )
 
 setup(**d)
